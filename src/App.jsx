dafa5f1ad104e120de8eb2752393eb8749862cc2
Nico <nicolas.limonoff@gmail.com>
2021-05-01 13:20:26 -0300

    login and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 import Login from "./components/Login";
 
 const App = () => {
   return (
     <div>
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" render={() => <Login />} />
         </Switch>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,18 @@ const Login = (props) => {
     <Container>
       <Content>
         <CTA>
-          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <Logos>
+            <ImageHulu src="/images/hulu.png" alt="" />
+            <ImageDisney src="/images/logo.svg" alt="" />
+            <ImageEspn src="/images/espn.png" alt="" />
+          </Logos>
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
         <BgImage />
       </Content>
@@ -56,17 +67,121 @@ const CTA = styled.div`
   flex-direction: column;
 `;
 
-const CTALogoOne = styled.img`
+const Logos = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items:center;
+  justify-content: center;
+
+`;
+
+const ImageDisney = styled.img`
   margin-bottom: 12px;
-  max-width: 600px;
-  min-height: 1px;
+  color: white;
+  max-width: 280px;
   display: block;
   width: 100%;
   opacity: 1;
-  ${CTA}:hover & {
+  transition: 0.5s ease;
+  animation-name: floating;
+  animation-duration: 3.4s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+  &:hover {
     transition: 0.5s ease;
     opacity: 0.5;
   }
 `;
 
+const ImageHulu = styled.img`
+  color: white;
+  max-width: 120px;
+  margin-top: 35px;
+  height: 40%;
+  display: block;
+  width: 100%;
+  opacity: 1;
+  transition: 0.5s ease;
+  animation-name: floating;
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+
+  &:hover {
+    transition: 0.5s ease;
+    opacity: 0.5;
+  }
+  @keyframes floating {
+  0% { transform: translate(0,  0px); }
+  50%  { transform: translate(0, 15px); }
+  100%   { transform: translate(0, -0px); }    
+}
+`;
+
+
+const ImageEspn = styled.img`
+  color: white;
+  max-width: 200px;
+  margin-top: 20px;
+  height: 100%;
+  display: block;
+  width: 100%;
+  opacity: 1;
+  transition: 0.5s ease;
+  animation-name: floating;
+  animation-duration: 2.4s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+  &:hover {
+    transition: 0.5s ease;
+    opacity: 0.5;
+  }
+  @keyframes floating {
+  0% { transform: translate(0,  0px); }
+  50%  { transform: translate(0, 15px); }
+  100%   { transform: translate(0, -0px); }    
+}
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: 0.5s ease;
+  &:hover {
+    background-color: #0483ee;
+    transition: 0.5s ease;
+  }
+  @keyframes floating {
+  0% { transform: translate(0,  0px); }
+  50%  { transform: translate(0, 15px); }
+  100%   { transform: translate(0, -0px); }    
+}
+`;
+
+const Description = styled.p`
+  color: #f9f9f9;
+  font-size: 11px;
+  font-weight:800;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  align-self: center;
+  width: 100%;
+`;
 export default Login;
